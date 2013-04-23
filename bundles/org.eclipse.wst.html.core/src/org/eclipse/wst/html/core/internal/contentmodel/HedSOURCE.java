82e9c07d8f50c60c8fc54e7601ff239dd840cefd
Salvador Zalapa <zalapa@mx1.ibm.com>
2013-04-23 11:55:58 -0500

    Bug 404733 - Wrong warning 'No end tag' for Source HTML5 tag

--- bundles/org.eclipse.wst.html.core/src/org/eclipse/wst/html/core/internal/contentmodel/HedSOURCE.java
+++ bundles/org.eclipse.wst.html.core/src/org/eclipse/wst/html/core/internal/contentmodel/HedSOURCE.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010 IBM Corporation and others.
+ * Copyright (c) 2010 - 2013 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -33,6 +33,7 @@ final class HedSOURCE extends HTMLElemDeclImpl {
 		super(HTML50Namespace.ElementName.SOURCE, collection);
 		typeDefinitionName = ComplexTypeDefinitionFactory.CTYPE_CDATA;
 		layoutType = LAYOUT_HIDDEN;
+		omitType = OMIT_END_DEFAULT;
 	}
 
 	/**
